5763f495a7b03abc5adfef85c1ba23fc2e446dc5
khashayar-bo <khashayarghavami@protonmail.com>
2026-04-26 14:14:31 +0200

    Add: is-subsequence - submission-4

---
+++ Data Structures & Algorithms/is-subsequence/submission-4.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isSubsequence(string& s, string& t) {
+        int sIndex = 0;
+
+        if (s.empty()) {
+            return true;
+        }
+
+        for (int i=0; i < t.size(); i++) {
+            if (t[i] == s[sIndex]) {
+                sIndex++;
+
+                if (sIndex == s.size()) {
+                    return true;
+                }
+            }
+        }
+
+        return false;
+    }
+};
